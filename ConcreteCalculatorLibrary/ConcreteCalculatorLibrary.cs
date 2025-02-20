180977ca7b0db98e16d2f149969acc250aa1e8b2
FC-Souza <fc-souza@outlook.com>
2025-02-20 12:26:45 -0500

    Add project files.

---
+++ ConcreteCalculatorLibrary/ConcreteCalculatorLibrary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConcreteCalculatorLibrary
+{
+    public enum CustomerType
+    {
+        NewResidential,
+        ExistingResidential,
+        Commercial
+    }
+
+    public class ConcreteCostCalculator
+    {
+        public static double CalculateCost(CustomerType customerType, double squareMeters)
+        {
+            double rate = GetRate(customerType);
+            return squareMeters * rate;
+        }
+
+        private static double GetRate(CustomerType customerType)
+        {
+            switch (customerType)
+            {
+                case CustomerType.NewResidential:
+                    return 30;
+                case CustomerType.ExistingResidential:
+                    return 25;
+                case CustomerType.Commercial:
+                    return 20;
+                default:
+                    throw new ArgumentException("Invalid customer type");
+            }
+        }
+    }
+}
